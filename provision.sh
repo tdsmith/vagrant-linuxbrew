3da0108e58c3ca6682b4d2477757453f15a18cc8
Tim D. Smith <git@tim-smith.us>
2015-08-23 14:30:29 -0700

    provision linuxbrew to vagrant user

--- provision.sh
+++ provision.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
-echo ". ~/.bashrc" > ~/.bash_profile
-echo "export PATH=~/.linuxbrew/bin:$PATH" >> ~/.bash_profile
-git clone https://github.com/Homebrew/linuxbrew ~/.linuxbrew
+sudo apt-get install -y build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev ruby-dev
+su -l vagrant <<EOS
+    printf '0a\nexport PATH=~/.linuxbrew/bin:\$PATH\n.\nw\n' | ed ~/.bashrc 2>&1 > /dev/null
+    [ -d ~/.linuxbrew ] || git clone https://github.com/Homebrew/linuxbrew ~/.linuxbrew
+EOS
